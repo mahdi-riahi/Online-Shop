5eb27f4c902a0d2ff2075b852d9449c9ea3dcbbf
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-10-01 13:35:38 +0330

    implement login and signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+class SignUpView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('home')
--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Authentication and Custom User
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
