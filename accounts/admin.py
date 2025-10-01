38e0f4889682116614be5bfa5f57d7e9b990ad01
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-10-01 12:59:57 +0330

    start project/ start app accounts/ create custom user

---
+++ accounts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    form = CustomUserChangeForm
+    # fieldsets = UserAdmin.fieldsets
+    add_form = CustomUserCreationForm
+    # add_fieldsets = None
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
