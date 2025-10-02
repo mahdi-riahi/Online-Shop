c11be73a2490d399c4c269603ba6db52fd6f36e1
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-10-02 16:07:58 +0330

    create model product

--- products/models.py
+++ products/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+
+class Product(models.Model):
+    # CATEGORIES = (
+    #     ('BK', 'Books'),
+    #     ('SM', 'Smart Phones'),
+    #     ('ED', 'Electronic Devices'),
+    # )
+    title = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    # category = models.CharField(choices=CATEGORIES, null=False, blank=True, max_length=2)
+    # cover = models.ImageField(upload_to=)
+    # storage = models.PositiveIntegerField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
