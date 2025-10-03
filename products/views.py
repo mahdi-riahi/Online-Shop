13de2b6f503c67873ed23f7046072f027a52d7d7
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-10-03 10:33:44 +0330

    implement product detail view, list view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(generic.ListView):
+    queryset = Product.objects.filter(active=True).order_by('-datetime_modified')
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+
+    # def get_queryset(self):
+    #     return Product.objects.filter(active=True).order_by('-datetime_modified')
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
